3a3b2ed6d0134d81e1634e6cfa3a775c4ae8f253
Christian Willians Ferraz Cintra <1901896@salas.aulas>
2020-03-09 19:52:07 -0300

    Dicionarios

---
+++ exdicionarios.py
@@ -0,0 +1,8 @@
+agenda = {"Maria": [99887766, 99887755],
+          "Pedro": [92345678],
+          "Joaquim": [99887711, 99665533]}
+
+print(agenda)
+
+agenda["Maria"] = 99991111  # altera valor
+print(agenda)
